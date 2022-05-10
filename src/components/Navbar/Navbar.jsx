03dc3c263584fd294e2c5eda62653c41bb370cbf
Sedana Yoga <9g.yoga27@gmail.com>
2022-05-10 08:22:57 +0800

    build: New Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,17 @@
 import Link from "next/link";
 import React from "react";
+import { useRouter } from "next/router";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import styles from "./Navbar.module.scss";
+import { capitalizeFirst } from "../../helper/textHelper";
 
+const menu = {
+  main: ["home", "about us", "browse"],
+  auth: ["log in", "register"],
+};
 const NavBar = () => {
+  const router = useRouter();
+  const currentPath = router.pathname;
   return (
     <Navbar expand="lg" className={styles.navBar}>
       <Container>
@@ -17,24 +25,36 @@ const NavBar = () => {
           <Nav
             className={`me-auto w-100 d-flex justify-content-between ${styles.navBar__menu}`}
           >
-            <div className="d-flex flex-lg-row flex-column gap-lg-2 gap-0 flex-grow-1 justify-content-end">
-              <Link href="/">
-                <a>home</a>
-              </Link>
-              <Link href="/aboutus">
-                <a>about us</a>
-              </Link>
-              <Link href="/browse">
-                <a>browse</a>
-              </Link>
+            <div className="d-flex flex-lg-row flex-column gap-lg-2 mt-3 mt-lg-0 gap-0 flex-grow-1 justify-content-end align-items-end">
+              {menu.main.map((mainMenu, index) => (
+                <Link
+                  key={index}
+                  href={`/${mainMenu === "home" ? "" : mainMenu}`}
+                >
+                  <a
+                    className={
+                      currentPath === `/${mainMenu === "home" ? "" : mainMenu}`
+                        ? styles.active
+                        : ""
+                    }
+                  >
+                    {mainMenu}
+                  </a>
+                </Link>
+              ))}
             </div>
             <div className="d-flex flex-lg-row flex-column flex-grow-1 justify-content-end">
-              <Link href="/login">
-                <a>login</a>
-              </Link>
-              <Link href="/register">
-                <a>register</a>
-              </Link>
+              {menu.auth.map((authMenu, index) => (
+                <Link key={index} href={`/${authMenu.replace(" ", "")}`}>
+                  <a
+                    className={`btn ${
+                      authMenu === "register" ? styles.register : styles.login
+                    } ${styles.authButton}`}
+                  >
+                    {capitalizeFirst(authMenu)}
+                  </a>
+                </Link>
+              ))}
             </div>
           </Nav>
         </Navbar.Collapse>
