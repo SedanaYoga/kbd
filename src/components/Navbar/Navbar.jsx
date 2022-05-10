913486cc17c384a2bed8a809e5a2ec6780de248c
Sedana Yoga <9g.yoga27@gmail.com>
2022-05-10 15:39:28 +0800

    fix: navbar responsiveness

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,55 +1,55 @@
-import Link from "next/link";
-import React from "react";
-import { useRouter } from "next/router";
-import { Navbar, Nav, Container } from "react-bootstrap";
-import styles from "./Navbar.module.scss";
-import { capitalizeFirst } from "../../helper/textHelper";
+import Link from 'next/link'
+import React from 'react'
+import { useRouter } from 'next/router'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import styles from './Navbar.module.scss'
+import { capitalizeFirst } from '../../helper/textHelper'
 
 const menu = {
-  main: ["home", "about us", "browse"],
-  auth: ["log in", "register"],
-};
+  main: ['home', 'about us', 'browse'],
+  auth: ['log in', 'register'],
+}
 const NavBar = () => {
-  const router = useRouter();
-  const currentPath = router.pathname;
+  const router = useRouter()
+  const currentPath = router.pathname
   return (
-    <Navbar expand="lg" className={styles.navBar}>
+    <Navbar expand='lg' className={styles.navBar}>
       <Container>
-        <Link href="/">
+        <Link href='/'>
           <a className={`nav-brand ${styles.navBar__brand}`}>
-            <img role="button" src="/images/logo.png" alt="logo" />
+            <img role='button' src='/images/logo.png' alt='logo' />
           </a>
         </Link>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav ">
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav '>
           <Nav
             className={`me-auto w-100 d-flex justify-content-between ${styles.navBar__menu}`}
           >
-            <div className="d-flex flex-lg-row flex-column gap-lg-2 mt-3 mt-lg-0 gap-0 flex-grow-1 justify-content-end align-items-end">
+            <div
+              className={`${styles.navBar__menu_main} d-flex flex-lg-row flex-column gap-lg-2 mt-3 mt-lg-0 gap-0 flex-grow-1 justify-content-end align-items-end`}
+            >
               {menu.main.map((mainMenu, index) => (
                 <Link
                   key={index}
-                  href={`/${mainMenu === "home" ? "" : mainMenu}`}
+                  href={`/${mainMenu === 'home' ? '' : mainMenu}`}
                 >
                   <a
-                    className={
-                      currentPath === `/${mainMenu === "home" ? "" : mainMenu}`
+                    className={`${currentPath === `/${mainMenu === 'home' ? '' : mainMenu}`
                         ? styles.active
-                        : ""
-                    }
+                        : ''
+                      }`}
                   >
                     {mainMenu}
                   </a>
                 </Link>
               ))}
             </div>
-            <div className="d-flex flex-lg-row flex-column flex-grow-1 justify-content-end">
+            <div className='d-flex flex-lg-row flex-column flex-grow-1 justify-content-end'>
               {menu.auth.map((authMenu, index) => (
-                <Link key={index} href={`/${authMenu.replace(" ", "")}`}>
+                <Link key={index} href={`/${authMenu.replace(' ', '')}`}>
                   <a
-                    className={`btn ${
-                      authMenu === "register" ? styles.register : styles.login
-                    } ${styles.authButton}`}
+                    className={`btn ${authMenu === 'register' ? styles.register : styles.login
+                      } ${styles.authButton}`}
                   >
                     {capitalizeFirst(authMenu)}
                   </a>
@@ -60,7 +60,7 @@ const NavBar = () => {
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  );
-};
+  )
+}
 
-export default NavBar;
+export default NavBar
