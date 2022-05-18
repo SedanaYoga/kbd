e0a63149fd243ff161d84a0acd57d9d73c74eea6
Sedana Yoga <9g.yoga27@gmail.com>
2022-05-18 09:06:06 +0800

    build: AboutUs page

--- src/pages/aboutus.jsx
+++ src/pages/aboutus.jsx
@@ -1,43 +1,59 @@
 import Head from 'next/head'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Col, Container, Row } from 'react-bootstrap'
 import UserLayout from '../components/Layouts/UserLayout'
+import styles from '../styles/pages/AboutUs.module.scss'
 
 export default function AboutUs() {
   return (
     <div>
+      <title>About Us - Kinta-Bali Dog</title>
       <Head>
-        <title>About Us - Kinta-Bali Dog</title>
         <meta
           name='description'
           content='About us - more about Kinta-Bali Dog Site'
         />
       </Head>
-      <Container className='heroContainer p-5'>
-        <h3 className='text-center'>Woof...</h3>
-        <Row>
-          <Col className='col-lg-4 col-12 text-lg-start text-center'>
-            <h1 style={{ fontWeight: 500, whiteSpace: 'nowrap' }}>About Us</h1>
-          </Col>
-          <Col className='col-lg-5 col-12 d-flex mt-4 text-lg-start text-center'>
-            <p className='text-start'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sem
-              turpis sed fusce feugiat est nisl, enim lobortis. Eu accumsan
-              adipiscing pharetra, ultricies mi neque sodales quam. Pretium, a,
-              ultricies integer consequat ornare mus scelerisque arcu proin.
-              Eleifend facilisis nulla eu porttitor.
-            </p>
-          </Col>
-        </Row>
-        <Row>
-          <Col className='w-100 mt-4 d-flex justify-content-lg-end justify-content-center'>
-            <img
-              className='heroImage'
-              src='/images/bg/back-3.png'
-              alt='about us'
-            />
-          </Col>
-        </Row>
-      </Container>
+
+      <div className={styles.aboutus}>
+        <Container>
+          <section className={styles.hero}>
+            <div className={styles.heroContent}>
+              <p>Woof...</p>
+              <h1>
+                About Our <span>Kennel</span>
+              </h1>
+              <p>
+                Hello to introduce, we are Kintamani Kennel,
+                <span>home of quality Kintamani dog breeds.</span>
+              </p>
+            </div>
+          </section>
+          <section className={styles.kennel}>
+            <Row>
+              <Col className={styles.kennelImage}>
+                <img src='/images/bg/back-4.png' alt='A Loving Kennel Image' />
+              </Col>
+              <Col className={styles.kennelInfo}>
+                <h1>
+                  A <span>Loving</span> Kennel 🐶
+                </h1>
+                <p>
+                  The Kintamani is an ancient breed of landrace dog that
+                  developed on the streets of Bali. Many of these dogs remain
+                  feral but they tend to be quite amicable towards humans and
+                  are often brought into the home as family pets and as
+                  watchdogs. They are extremely territorial and don’t generally
+                  get along with other dogs or other animals, but they are also
+                  extremely devoted and can display intense loyalty towards
+                  their chosen owner. While these dogs make excellent watchdogs,
+                  they are often too vocal and energetic to make appropriate
+                  roommates in a small home or apartment type setting.
+                </p>
+              </Col>
+            </Row>
+          </section>
+        </Container>
+      </div>
     </div>
   )
 }
